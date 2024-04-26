66d0b6302572afdfd23381c265ba5fbac61ad482
syeda <sbtshakeel61@gmail.com>
2024-04-26 23:43:26 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+let dayOff = "sunday"
+
+switch(dayOff){
+
+    case "saturday":
+console.log("go to hiking")
+break;
+
+case "sunday":
+    console.log ("read a book")
+   break;
+   
+   default:
+    console.log ("work on hobby")
+}
